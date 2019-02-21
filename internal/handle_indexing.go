2b21e729a36867d4892dcbb086c92a6230291792
brookmezgu <brookmezgebu64@gmail.com>
2019-02-21 22:31:05 +0300

    + implemented functions to handle the indexing part

---
+++ internal/handle_indexing.go
@@ -0,0 +1,92 @@
+package internal
+
+import (
+	"bufio"
+	"encoding/csv"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
+)
+
+func AvailableFileSystems() []string {
+	return getDrives()
+}
+
+func getDrives() (r []string) {
+	for _, drive := range "ABCDEFGHIJKLMNOPQRSTUVWXYZ" {
+		_, err := os.Open(string(drive)+":\\")
+		if err == nil {
+			r = append(r, string(drive))
+		}
+	}
+	return r
+}
+
+
+func GetRefreshedMusicList(drives []string) MusicFiles {
+	// start
+	cmd := exec.Command("indexing.exe")
+	if err := cmd.Start(); err != nil {
+		panic(err)
+	}
+
+	// wait or timeout
+	donec := make(chan error, 1)
+	go func() {
+		donec <- cmd.Wait()
+	}()
+	select {
+	case <-time.After(25 * time.Second):
+		return MusicFiles{}
+	case <-donec:
+		os.Remove("allMusicFiles.csv")
+		os.Rename("allMusicFiles.csv" , "allMusicFiles.csv")
+		return GetMusicList(drives)
+	}
+}
+
+func GetMusicList(drives []string) MusicFiles {
+	csvFile, _ := os.Open("allMusicFiles.csv")
+	reader := csv.NewReader(bufio.NewReader(csvFile))
+
+	var musicFiles MusicFiles
+	for {
+		line, e := reader.Read()
+		if e == io.EOF {
+			break
+		} else if e != nil {
+			log.Fatal(e)
+		}
+
+		if SliceContains(drives , string(strings.ToUpper(line[1])[0])) {
+			musicFiles = append(musicFiles, MusicStruct{
+				Fname: line[0],
+				Fpath: line[1],
+			})
+		}
+
+	}
+
+	return musicFiles
+}
+
+func SliceContains (a []string , i string) bool {
+	for _ , value := range a {
+		if string(value) == i {
+			return true
+		}
+	}
+	return false
+}
+
+func FileExt (filename string) string {
+	if len(filename) == 0 {
+		return ""
+	}
+
+	dotIndex := strings.LastIndex(filename , ".")
+	return filename[dotIndex:]
+}
